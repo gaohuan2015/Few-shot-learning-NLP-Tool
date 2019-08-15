61f3fe0f566a6bbe95d0796250e45f9b1bffe7b7
Huan <53857658@qq.com>
2019-08-15 14:09:48 +0800

    add data process class

---
+++ Relation Extraction/FewRel.py
@@ -0,0 +1,24 @@
+import json
+
+
+class FewRel:
+    def __init__(self):
+        self.train_path = "Data/Few Rel/train.json"
+        self.val_path = "Data/Few Rel/val.json"
+
+    def loadJson(self, name):
+        file_path = ""
+        if name == "train":
+            file_path = self.train_path
+        elif name == "validation":
+            file_path == self.val_path
+        else:
+            raise Exception("[ERROR] data file doesn't exist")
+        with open(file_path, "r", "utf-8") as f:
+            original_data = json.load(f)
+            for relation in original_data:
+                sentences_number += len(original_data[relation])
+
+if __name__ == "__main__":
+    data = FewRel()
+    data.loadJson('train')
